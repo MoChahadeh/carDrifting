f82c67a3e9897ef68eed1a5e6faf0d87d6ba6f28
Mo Chahadeh <applednd2002@gmail.com>
2022-07-15 15:53:19 +0300

    added car and key events

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from settings import *
+from car import *
 
 
+car = Car()
 
 while True:
 
@@ -11,7 +13,25 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
+            elif event.key == pygame.K_RIGHT:
+                car.inst_thrust.x = THRUST
+            elif event.key == pygame.K_LEFT:
+                car.inst_thrust.x = -THRUST
+            elif event.key == pygame.K_UP:
+                car.inst_thrust.y = -THRUST
+            elif event.key == pygame.K_DOWN:
+                car.inst_thrust.y = THRUST
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                car.inst_thrust.x = 0
+            elif event.key == pygame.K_LEFT:
+                car.inst_thrust.x = 0
+            elif event.key == pygame.K_UP:
+                car.inst_thrust.y = 0
+            elif event.key == pygame.K_DOWN:
+                car.inst_thrust.y = 0
 
     WINDOW.fill(BG)
+    car.update()
     pygame.display.update()
     CLOCK.tick(FPS)
