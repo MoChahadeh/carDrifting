9cfb9ea5636c4823869874f4be87a322272e711d
Mo Chahadeh <applednd2002@gmail.com>
2022-07-15 15:17:31 +0300

    added settings

---
+++ settings.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+WIDTH, HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
+CLOCK = pygame.time.Clock()
+FPS = 60
+
+BG = (100,100,100)
+
+pygame.display.set_caption("Car Drifter")
+
+THRUST = 10
